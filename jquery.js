ac67ed7bbbaa00272f28e94fbb0d9afa55024360
Maikol Zapata <nucleo@Mac-mini-de-Maikol.local>
2021-09-06 17:49:35 -0300

    este es mi segundo commit

---
+++ jquery.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $('#foto1').click(function() {
+        $('#foto1').attr('src','img/cat0.png');
+    })
+
+    $('#foto2').click(function() {
+        $('#foto2').attr('src','img/cat1.png');
+    })
+
+    $('#foto3').click(function() {
+        $('#foto3').attr('src','img/cat2.png');
+    })
+
+    $('#foto4').click(function() {
+        $('#foto4').attr('src','img/cat3.png');
+    })
+
+    $('#foto5').click(function() {
+        $('#foto5').attr('src','img/cat4.png');
+
+    })
+})
